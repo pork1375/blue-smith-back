8b1dcad0f6483e506960b70d478540a76c38d2fc
chansung <pork1375@gmail.com>
2022-06-25 12:28:14 +0900

    merge

---
+++ src/main/java/com/example/blueSmith/BlueSmithApplication.java
@@ -0,0 +1,13 @@
+package com.example.blueSmith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueSmithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueSmithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blueSmith/BlueSmithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blueSmith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueSmithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
